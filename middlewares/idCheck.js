6589aed266b24e34ff3193c5476a0deab84fb26a
Nikita P <94718260+Akvigi@users.noreply.github.com>
2022-11-02 22:39:59 +0200

    Update idCheck.js

--- middlewares/idCheck.js
+++ middlewares/idCheck.js
@@ -2,7 +2,6 @@ const { isValidObjectId } = require('mongoose');
 const createError = require('http-errors');
 
 const idCheck = (req, res, next) => {
-  console.log(req.params.contactId);
   const result = isValidObjectId(req.params.contactId);
   if (!result) {
     throw createError(400, `Invalid format id`);
